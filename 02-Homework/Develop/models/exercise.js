e32e145b16abe1ecb1d96a4177c6d0cf4c37f79c
Michael Cassaro <mdcassaro@hotmail.com>
2020-05-06 19:36:14 -0500

    server.js finished and workout database finished

--- 02-Homework/Develop/models/exercise.js
+++ 02-Homework/Develop/models/exercise.js
@@ -2,8 +2,15 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const ExcersiseSchema = new Schema({
-  title: String,
-  body: String
+  type: String,
+  name: String,
+  totalDistance: Number,
+  totalDuration: Number,
+  totalReps: Number,
+  totalSets: Number,
+  numExercise: Number,
+  totalWeight: Number,
+
 });
 
 const Excercise = mongoose.model("Exercise", ExcersiseSchema);
